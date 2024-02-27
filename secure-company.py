2936170481715c7c71fc1cd3f3b948eb69199e08
Jhosef <jmperezperez25@gmail.com>
2024-02-27 03:53:36 -0500

    finished

--- secure-company.py
+++ secure-company.py
@@ -2,7 +2,6 @@ import math as mt
 import os
 import time
 
-
 def factorial(n):
     
     if n==1 or n == 0:
@@ -14,74 +13,39 @@ def factorial(n):
         try:
             result *= i + 1
         except:
-            result = 10e308
+            result = 10e307
     
     return result
 
-def reclamation_distribution1( n ): # M
+def reclamation_distribution( n ): # M
     
-    try:
-        res= (1/2)**n
-        return res
-    except:
-        return 10e-308
+    result=0
+    for i in range(n):
+        result+= (factorial(i)*factorial(n-i))/factorial(n)
     
+    return result
+        
+def amount_reclamation_distribution( n ): # number of reclamations
+    
+    return int(reclamation_distribution(n)*n)
 
-def reclamation_distribution2(xi):
-    
-    if xi == 0:
-        xi+=1
-    
-    return 1-(1/xi)
-        
-def amount_reclamation_distribution( n ): # F
-    
-    complaint_number=0
-    
-    for xi in range(n):
-        
-        if (n - (xi+1)) % 2 == 0: # people complaint if half of clients complaint
-                
-            try:
-                combinations= int(int(factorial(n)/ (factorial(xi+1)*factorial(n-(xi+1))))) 
-            except:
-                combinations=10e308
-            
-            recl = reclamation_distribution1(n) 
-            
-            pq=( recl** (xi+1) )*(( 1 - recl )**( n - (xi+1) )) # number of complaits for a client that complaints with probability of p
-            
-            complaint_number += int(combinations*pq)
-        
-        else:
-            
-            try:
-                combinations= (int(factorial(n)/ (factorial((xi+1))*factorial(n-(xi+1))))) # people complaint if there are many clients
-            except:
-                combinations=10e308
-                
-            recl = reclamation_distribution2((xi+1)) 
-            
-            pq=( recl** (xi+1) )*(( 1 - recl )**( n - (xi+1) )) # number of complaits for a client that complaints with probability of p
-            
-            complaint_number += int(combinations*pq)
-            
-    return complaint_number
-
-def time_client_arrive( amount_reclamations ): # v
+def time_client_arrive( amount_reclamations ): # time for clients to arrive
 
         if amount_reclamations < 2:
             return 1
         
         return int(mt.log2(amount_reclamations))
 
-def number_client_time( n , reclamation_number , time): # u
+def number_client_time( n , reclamation_number , time): # number of clients leaving
     
     client_left=client_time( reclamation_number , n)
     
-    return int( client_left*time )    
+    if int( client_left*time ) == 0:
+        return 1
+    else:
+        return int( client_left*time )
     
-def client_time( reclamation_number , n ):
+def client_time( reclamation_number , n ): # distribution for a client to leave
     
     if n % 2 == 0:
         
@@ -106,29 +70,22 @@ def company( n0 , a0 , capital ):
         n_in: total number of client assisted
         reclamation_number: total number of complaints
     '''
-    
-    last_time=0
+
     start = time.time()
     while n != 0:
         
         arrive_time = time_client_arrive( amount_reclamations = reclamation_number )
+                
+        n += 1
+        n_in +=1  
+                    
+        a0 += n * capital                
         
-        if t-last_time >= arrive_time: 
-            n += 1
-            n_in +=1  
-            last_time = t
+        reclamation_number = amount_reclamation_distribution( n )        
         
-        a0 += n * capital        
+        total_reclamation += reclamation_number        
         
-        
-        reclamation_number = amount_reclamation_distribution( n )
-        
-        
-        total_reclamation += reclamation_number
-        
-        
-        cleft= number_client_time(n , reclamation_number , t - last_time)
-
+        cleft= number_client_time(n , reclamation_number , arrive_time)
 
         n-=cleft
     
@@ -149,7 +106,7 @@ def company( n0 , a0 , capital ):
             start +=1
     pass
 
-company( 100 , 500 , .1 )
+company( 1000 , 500 , .1 )
 
 
 
